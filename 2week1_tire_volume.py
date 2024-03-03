12437f47027c27e505d195a02864dfb947f25034
VtrMomesso <vtrmomesso@gmail.com>
2024-03-02 23:58:07 -0300

    Update 2week1_tire_volume.py

--- 2week1_tire_volume.py
+++ 2week1_tire_volume.py
@@ -12,7 +12,7 @@ import math
 
 
 # Get the Radio, Width and Diameter from the user.
-width = float(input("Enter the width of the tire in mn (ex 205): "))
+width = float(input("Enter the width of the tire in mm (ex 205): "))
 aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
 diameter = float(input("Enter ther diameter of the wheel in inches (ex 15): "))
 
