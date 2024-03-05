155912d126594725f0870e7295e9e4fb0ab5c696
VtrMomesso <vtrmomesso@gmail.com>
2024-03-05 00:55:27 -0300

    Update 1week5.py

--- 1week5.py
+++ 1week5.py
@@ -10,7 +10,7 @@ print("Adding the items that you prefer and its prices. (Please, follow the MENU
 # Variable Declaration 
 products_list = []
 price_list = []
-priducts = ""
+products = ""
 price = ""
 
 # Beginning of code, with the While loop
@@ -51,4 +51,17 @@ while True:
                 products_list.append(products)
                 price_list.append(price)
 
-
+    # The second option make a list and show to the user.
+    elif option == "2":
+        print()
+        print("The items in your cart list is: ")
+        # For loop, search in the list all the values that was inputed.
+        for i in range(len(products_list)):
+            #  Especifid the index and add one more, each time this 
+            # loop run it brings the following result and save in 
+            # the products' and prices' list.
+            index = i + 1
+            products = products_list[i]
+            price = price_list[i]
+            # Print the requested informations.
+            print(f"{index}. {products} ${price:.2f}")
