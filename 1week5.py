3ce9f291d371d96c6f09c5538b2d97f783fa85d6
VtrMomesso <vtrmomesso@gmail.com>
2024-03-05 01:43:06 -0300

    Update 1week5.py

--- 1week5.py
+++ 1week5.py
@@ -93,10 +93,20 @@ while True:
         print("Item removed ")
         # After this information, come back to menu.
 
-
     # The Fourth option show us the sum of the total 
     elif option == "4":
         # The print bring to the user the results.
         # The format show us what informations will be printed
         # The sum make the calculations with all the items in the price list
         print("The total is: ${:.2f}".format(sum(price_list)))
+        # After computing the total bring back to the menu. 
+
+    # The Fifth option stop the program and close it. 
+    elif option == "5":
+        # Print a greatfulness and a bye greeting.
+        print("Thank you. Goodbye. ")
+        # Jump a line
+        print()
+        # Stop the program and close it.
+        break;
+    
