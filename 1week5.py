05d7711953e0bec3f59e9cddb3df2828af3d910e
VtrMomesso <vtrmomesso@gmail.com>
2024-03-04 22:42:03 -0300

    Create 1week5.py

---
+++ 1week5.py
@@ -0,0 +1,15 @@
+
+# Shopping Cart Project
+
+# By Victor Dos Santos 
+
+# Explanation about the program.
+print("Wlcome, here you can creat your shopping cart. ")
+print("Adding the items that you prefer and its prices. (Please, folow the MENU)")
+
+# Variable Declaration 
+product_list = []
+price_list = []
+priducts = ""
+price = ""
+
