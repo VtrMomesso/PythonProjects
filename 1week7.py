7fca4caf7ef87a8b3f0111efa558b846ffcb0a12
VtrMomesso <vtrmomesso@gmail.com>
2024-03-08 23:47:18 -0300

    Update 1week7.py

--- 1week7.py
+++ 1week7.py
@@ -1,10 +1,19 @@
 """
+Project 07: Windchill Calculator
+
 Author: Victor Dos Santos
 
-Project 07: Windchill Calculator.
+
 """
+
 
 #Convert Celsius temperature to Fahrenheit using a function.
 def celsius_to_fahrenheit(celsius):
    
     return (celsius * 9/5) + 32
+
+#Calculate wind chill using the formula for Fahrenheit.
+def calculate_wind_chill(temperature, wind_speed):
+    
+    wind_chill = 35.74 + 0.6215 * temperature - 35.75 * (wind_speed ** 0.16) + 0.4275 * temperature * (wind_speed ** 0.16)
+    return wind_chill
