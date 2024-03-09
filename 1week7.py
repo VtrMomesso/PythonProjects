0362c0f97b93d8c3deabe478011aba0e1e9619da
VtrMomesso <vtrmomesso@gmail.com>
2024-03-08 23:45:54 -0300

    Update 1week7.py

--- 1week7.py
+++ 1week7.py
@@ -3,3 +3,8 @@ Author: Victor Dos Santos
 
 Project 07: Windchill Calculator.
 """
+
+#Convert Celsius temperature to Fahrenheit using a function.
+def celsius_to_fahrenheit(celsius):
+   
+    return (celsius * 9/5) + 32
