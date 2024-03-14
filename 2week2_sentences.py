3512a846b9ba1e827bed99b633e8c8b03df740a4
VtrMomesso <vtrmomesso@gmail.com>
2024-03-13 23:30:21 -0300

    Create 2week2_sentences.py

---
+++ 2week2_sentences.py
@@ -0,0 +1,38 @@
+
+import random
+
+
+
+def main():
+
+
+
+def make_sentence():
+
+
+def get_determiner(quantity):
+    """Return a randomly chosen determiner. A determiner is
+    a word like "the", "a", "one", "some", "many".
+    If quantity is 1, this function will return either "a",
+    "one", or "the". Otherwise this function will return
+    either "some", "many", or "the".
+
+    Parameter
+        quantity: an integer.
+            If quantity is 1, this function will return a
+            determiner for a single noun. Otherwise this
+            function will return a determiner for a plural
+            noun.
+    Return: a randomly chosen determiner.
+    """
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["some", "many", "the"]
+
+    # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
+
+def get_noun():
+    return
