26a45d352cee06fa0329a3c98b257c109b8a4715
VtrMomesso <vtrmomesso@gmail.com>
2024-03-14 01:47:43 -0300

    Update 2week2_sentences.py

--- 2week2_sentences.py
+++ 2week2_sentences.py
@@ -144,6 +144,44 @@ def get_verb(quantity, tense):
     verb = random.choice(verbs)
     return verb
 
+def get_preposition():
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
+
+    Return: a randomly chosen preposition.
+    """
+    prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+    
+    preposition = random.choice(prepositions)
+    return preposition
+
+def get_preposition_phrase(quantity):
+    """Build and return a prepositional phrase composed
+    of three words: a preposition, a determiner and a 
+    noun by calling the get_preposition, get_determiner,
+    and get_noun functions.
+
+    Parameter
+        quantity: an interger that determines if the
+        determiner and noun in the prepositional 
+        phrase returned from this function should 
+        be single or plural.
+    Return: a prepositional phrase.
+    """
+    
+
+
 
 
 if __name__ == "__main__":
