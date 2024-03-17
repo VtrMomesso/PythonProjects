1dcbc7c8586ce522c4f3a9c290a2d2c99bc736d2
VtrMomesso <vtrmomesso@gmail.com>
2024-03-17 00:17:56 -0300

    Create words.py

---
+++ 2week3_test/words.py
@@ -0,0 +1,20 @@
+# Victor dos Santos
+
+# Using functions and learning how to test that
+
+# Copyright 2024, BYU-I. All rights reserved.
+
+def prefix(string1, string2):
+    """Return the prefix, if any, that appears in both string1 and
+    string2. In other words, return a string of the characters
+    that appear at the beginning of both string1 and string2. For 
+    example, if string1 is "inconceivable" and string2 is 
+    "inconvenient", this function will return "incon".
+
+    Parameters
+        string1: a string of text.
+        string2: another string of text.
+    Return: a string
+    """
+
+    
