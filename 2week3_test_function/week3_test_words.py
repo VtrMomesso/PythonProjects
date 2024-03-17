dd622ee329d6cd9cf7b3785fc6c08afcc8664116
VtrMomesso <vtrmomesso@gmail.com>
2024-03-17 00:44:52 -0300

    Create week3_test_words.py

---
+++ 2week3_test_function/week3_test_words.py
@@ -0,0 +1,12 @@
+"""Verify that the prefix and suffix functions work correctly."""
+
+from week3_words import prefix, suffix
+import pytest
+
+
+
+def test_prefix():
+      """Verify that the prefix function works correctly.
+    Parameters: none
+    Return: nothing
+    """
