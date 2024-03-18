4b299dbff82b81e3fa1825ac4c9cac90a26581b5
VtrMomesso <vtrmomesso@gmail.com>
2024-03-18 03:26:38 -0300

    Create water_flow.py

---
+++ 2week3_test_function/water_flow.py
@@ -0,0 +1,16 @@
+
+
+
+def water_column_height(tower_height, tank_height):
+    """This function calculates and return the the height
+    tower of the city, the calculations is made as the 
+    following formula:  h = t + ((3 * w) / 4)
+    Where H = column_height, T = tower_height and W = tank_height
+    
+    Parameter: 
+        tower_height: float number
+        tank_height: float number
+    Return: column_height
+    """
+    column_height = tower_height + ((3 * tank_height) / 4)
+    return column_height
