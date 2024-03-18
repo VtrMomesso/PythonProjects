cc48b0735158784b15fa151db7aa0b0b0a53c04d
VtrMomesso <vtrmomesso@gmail.com>
2024-03-18 05:18:44 -0300

    Update test_water_flow.py

--- 2week3_test_function/test_water_flow.py
+++ 2week3_test_function/test_water_flow.py
@@ -13,7 +13,7 @@ Copyrights 2024 - By Victor dos Santos - BYU-I
 """
 
 # Import the necessaries files to work this project
-from water_flow import water_column_height, pressure_gain_from_water_height 
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
 from pytest import approx
 import pytest
 
@@ -39,4 +39,8 @@ def test_pressure_gain_from_water_height():
     assert pressure_gain_from_water_height(50.0) == approx(489.450)
 
 
+def test_pressure_loss_from_pipe():
+
+    assert pressure_loss_from_pipe(0.048692, 0.00, 0.018, 1.75) == approx(0.00)
+    
 pytest.main(["-v", "--tb=line", "-rN", __file__])
