0cb0cfeb1a0bcf46a72aa626e9d78c0c6622e25f
VtrMomesso <vtrmomesso@gmail.com>
2024-03-18 03:43:44 -0300

    Update test_water_flow.py

--- 2week3_test_function/test_water_flow.py
+++ 2week3_test_function/test_water_flow.py
@@ -1,2 +1,18 @@
+from water_flow import tower_height, tank_height
 from pytest import approx
 import pytest
+
+def test_water_column_height():
+    column_height = water_column_height(0.0, 0.0)
+    assert column_height == approx(0.0)
+
+    column_height = water_column_height(0.0, 10.0)
+    assert column_height == approx(7.5)
+
+    column_height = water_column_height(25.0, 0.0)
+    assert column_height == approx(7.5)
+
+    column_height = water_column_height(48.3, 12.8)
+    assert column_height == approx(57.9)
+
+pytest.main(["-v", "--tb=line", "-rN", __file__])
