24cbc76b0c0bac4bd9798335367b642399fee0b1
VtrMomesso <vtrmomesso@gmail.com>
2024-03-18 23:58:20 -0300

    Update test_water_flow.py

--- 2week3_test_function/test_water_flow.py
+++ 2week3_test_function/test_water_flow.py
@@ -13,22 +13,21 @@ Copyrights 2024 - By Victor dos Santos - BYU-I
 """
 
 # Import the necessaries files to work this project
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings
+from water_flow import pressure_loss_from_fittings
 from pytest import approx
 import pytest
 import math
 
 
-def test_water_column_height():
-    """Verify that the water_column_height function works correctly.
+#def test_water_column_height():
+"""Verify that the water_column_height function works correctly.
 
     Parameters: 
         tower_height, 
         tank_height.
     Return: nothing
     """
-
-    column_height = water_column_height(0.0, 0.0)
+""" column_height = water_column_height(0.0, 0.0)
     assert column_height == approx(0.0)
 
     column_height = water_column_height(0.0, 10.0)
@@ -38,24 +37,24 @@ def test_water_column_height():
     assert column_height == approx(25.0)
 
     column_height = water_column_height(48.3, 12.8)
-    assert column_height == approx(57.9)
+    assert column_height == approx(57.9)"""
 
 
-def test_pressure_gain_from_water_height():
-    """Verify that the pressure_gain_from_water_height function works correctly.
+#def test_pressure_gain_from_water_height():
+"""Verify that the pressure_gain_from_water_height function works correctly.
 
     Parameters: 
         height
     Return: nothing
     """
 
-    assert pressure_gain_from_water_height(0) == approx(0.00)
+"""  assert pressure_gain_from_water_height(0) == approx(0.00)
     assert pressure_gain_from_water_height(30.2) == approx(295.628)
-    assert pressure_gain_from_water_height(50.0) == approx(489.450)
+    assert pressure_gain_from_water_height(50.0) == approx(489.450)"""
 
 
-def test_pressure_loss_from_pipe():
-    """Verify that the pressure_loss_from_pipe function works correctly.
+#def test_pressure_loss_from_pipe():
+"""Verify that the pressure_loss_from_pipe function works correctly.
 
     Parameters: 
         pipe_diameter,
@@ -66,7 +65,7 @@ def test_pressure_loss_from_pipe():
     """
 
     #This variable helps on the test statments, and brings a tolerance by error
-    tolerance = 0.001
+"""  tolerance = 0.001
 
     # The if statiment helps we find the condition where the code will work.
     if abs(0) < tolerance:
@@ -77,7 +76,7 @@ def test_pressure_loss_from_pipe():
         assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.75) == approx(-113.008, abs=tolerance)
         assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.65) == approx(-100.462, abs= tolerance)
         assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, abs=tolerance)
-        assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, abs=tolerance)
+        assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, abs=tolerance)"""
 
 
 def test_pressure_loss_from_fittings():
@@ -92,7 +91,7 @@ def test_pressure_loss_from_fittings():
     #This variable helps on the test statments, and brings a tolerance by error
     tolerance = 0.001
 
-    if abs(0) < tolerance:
+    if abs(0.0) < tolerance: 
 
         assert pressure_loss_from_fittings(0.00, 3) == approx(0.000, abs=tolerance)
         assert pressure_loss_from_fittings(1.65, 0) == approx(0.00, abs=tolerance)
