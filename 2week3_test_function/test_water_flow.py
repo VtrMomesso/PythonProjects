e2b29afb5af2cd96345e9d3a14de5040868b54c0
VtrMomesso <vtrmomesso@gmail.com>
2024-03-18 21:38:39 -0300

    Update test_water_flow.py

--- 2week3_test_function/test_water_flow.py
+++ 2week3_test_function/test_water_flow.py
@@ -16,6 +16,7 @@ Copyrights 2024 - By Victor dos Santos - BYU-I
 from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
 from pytest import approx
 import pytest
+import math
 
 
 def test_water_column_height():
@@ -41,11 +42,17 @@ def test_pressure_gain_from_water_height():
 
 def test_pressure_loss_from_pipe():
 
-    assert pressure_loss_from_pipe(0.048692, 0.00, 0.018, 1.75) == approx(0.00)
-    assert pressure_loss_from_pipe(0.048692, 200.00, 0.00, 1.75) == approx(0.00)
-    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 0.00) == approx(0.00)
-    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.75) == approx(-113.008)
-    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.65) == approx(-100.462)
-    assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576)
-    assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884)
+    tolerance = 0.001
+
+    if abs(-61.576) < tolerance:
+
+        assert pressure_loss_from_pipe(0.048692, 0.00, 0.018, 1.75) == approx(0.00, abs=tolerance)
+        assert pressure_loss_from_pipe(0.048692, 200.00, 0.00, 1.75) == approx(0.00, abs=tolerance)
+        assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 0.00) == approx(0.00, abs=tolerance)
+        assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.75) == approx(-113.008, abs=tolerance)
+        assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.65) == approx(-100.462, abs= tolerance)
+        assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, abs=tolerance)
+        assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, abs=tolerance)
+
+
 pytest.main(["-v", "--tb=line", "-rN", __file__])
