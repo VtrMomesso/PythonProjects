d19dccf8c5d745f3a0288d23921dbb122b9fab3e
VtrMomesso <vtrmomesso@gmail.com>
2024-03-19 02:06:56 -0300

    Update water_flow.py

--- 2week3_test_function/water_flow.py
+++ 2week3_test_function/water_flow.py
@@ -189,5 +189,9 @@ def main():
     velocity = HOUSEHOLD_VELOCITY
     loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
     pressure += loss
-    
+
     print(f"Pressure at house: {pressure:.1f} kilopascals")
+
+
+if __name__ == "__main__":
+    main()
