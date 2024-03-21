b325ebcec03d9f3a1fc5d14297d517e3e3efa840
VtrMomesso <vtrmomesso@gmail.com>
2024-03-21 17:26:55 -0300

    Update 2week4_vehicles.py

--- 2week4_vehicles.py
+++ 2week4_vehicles.py
@@ -7,6 +7,15 @@ def main():
     a list that contains the year, manufacturer, model, color, 
     engine design, and engine displacement.
     """
+    vehicles_dict ={
+        # VIN: [year, manufacturer, model, color, eng_design, eng_displace]
+        "1J4GL48K4UF993861": [2002, "Jeep", "Liberty", "blue", "V6", 3.7],
+        "1YVGF22C8AN381568": [2002, "Mazda", "626", "white", "I4", 2.0],
+        "WP0AA0926HG410293": [1987, "Porsche", "924S", "red", "I4", 2.5],
+        "5TDZA23CXTU102983": [2006, "Toyota", "Sienna", "gold", "V6", 3.3],
+        "1GKKVRED5ZL382610": [2011, "GMC", "Acadia", "charcoal", "V6", 3.5],
+        "2T3BF4DV9QR146782": [2012, "Toyota", "RAV 4", "green", "I4", 2.5]
+    }
 
 if __name__ == "__main__":
     main()
