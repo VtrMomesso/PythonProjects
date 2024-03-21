fb2e5c9a22d236527451c7093a02eebb43e4ead2
VtrMomesso <vtrmomesso@gmail.com>
2024-03-20 23:34:01 -0300

    Update random_numbers.py

--- 2week4_test_function/random_numbers.py
+++ 2week4_test_function/random_numbers.py
@@ -13,13 +13,13 @@ def main():
     numbers = [16.2, 75.1, 52.3]
     print(f"Numbers {numbers}")
 
-    # Call the append_ random_numbers functions
+    # Call the append_ random_numbers function
     # with one arguments to add one random number
     # to the numbers list.
     append_random_numbers(numbers)
     print(f"Numbers {numbers}")
 
-    # Call the append_ random_numbers functions
+    # Call the append_ random_numbers function
     # with one arguments to add three random number
     # to the numbers list.
     append_random_numbers(numbers)
@@ -27,6 +27,11 @@ def main():
     
     # Create a list to store random words.
     words = []
+
+    # Call the append_random_words function
+    # with with one argument to add one random
+    # word to the list.
+     
 
 
 def append_random_numbers():
