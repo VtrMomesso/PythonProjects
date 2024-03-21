f04535b5257e5cd7697e414f8894ca4133ed9ff9
VtrMomesso <vtrmomesso@gmail.com>
2024-03-21 17:25:46 -0300

    Update 2week4_vehicles.py

--- 2week4_vehicles.py
+++ 2week4_vehicles.py
@@ -1,7 +1,12 @@
 # Copyrights 2024 | By Victor dos Santos 
 
 def main():
-
+    """ Create a dictionary that contanins about six vehicles.
+    The key for each vehicle in the dictionary is the vehicle's
+    indentification number (VIN). The value for each vehicle is
+    a list that contains the year, manufacturer, model, color, 
+    engine design, and engine displacement.
+    """
 
 if __name__ == "__main__":
     main()
