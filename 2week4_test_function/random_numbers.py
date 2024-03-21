e18a1e4a0deee2e9d4d789fde372d03249a1d385
VtrMomesso <vtrmomesso@gmail.com>
2024-03-20 23:23:03 -0300

    Create random_numbers.py

---
+++ 2week4_test_function/random_numbers.py
@@ -0,0 +1,8 @@
+# Copyrights 2024 - By Victor dos Santos
+
+# Random Number With functions
+
+"""
+Write and call functions that demonstrate both
+default parameter values and pass by reference.
+"""
