96dff2988e8e62eef3cd141694c33bab75a61fef
VtrMomesso <vtrmomesso@gmail.com>
2024-03-22 13:02:00 -0300

    Update formula.py

--- 2week4_test_function/formula.py
+++ 2week4_test_function/formula.py
@@ -6,4 +6,8 @@ class FormulaError(ValueError):
 
 
 def parse_formula(formula, periodic_table_dict):
-    """"""
+    """Convert a chemical formula for a molecule into a compound
+    list that stores the quantity of atoms of each element
+    in the molecule. For example, this function will convert
+    "H2O to [["H", 2], ["O", 1]] and 
+    "PO4H2(CH2)12CH3" to [["P', 1], ["O", 4], ["H", 29], ["C", 13]]"""
