d6b381c5a96ec8258b4d0f48ef94d8715ba12ab1
VtrMomesso <vtrmomesso@gmail.com>
2024-03-22 13:05:46 -0300

    Update formula.py

--- 2week4_test_function/formula.py
+++ 2week4_test_function/formula.py
@@ -10,4 +10,12 @@ def parse_formula(formula, periodic_table_dict):
     list that stores the quantity of atoms of each element
     in the molecule. For example, this function will convert
     "H2O to [["H", 2], ["O", 1]] and 
-    "PO4H2(CH2)12CH3" to [["P', 1], ["O", 4], ["H", 29], ["C", 13]]"""
+    "PO4H2(CH2)12CH3" to [["P', 1], ["O", 4], ["H", 29], ["C", 13]]
+    
+    Parameters:
+        formula: is a string that contains a chemical formula.
+        periodic_table_dict: is the compound dictionary returned
+            from make_periodic_table.
+    Return: a compound list that contains chemical symbol and
+        quantities like  this [["Fe", 2], ["O", 3]]
+    """
