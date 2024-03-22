f559234e2c638dc3f4a46606b842a5721d6798f2
VtrMomesso <vtrmomesso@gmail.com>
2024-03-22 12:55:42 -0300

    Update formula.py

--- 2week4_test_function/formula.py
+++ 2week4_test_function/formula.py
@@ -1,4 +1,9 @@
+
 class FormulaError(ValueError):
     """FormulaError is the type of error that the parse_formula
     function will raise if a formula is invalid.
     """
+
+
+def parse_formula(formula, periodic_table_dict):
+    """"""
