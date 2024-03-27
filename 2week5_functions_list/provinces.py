e7de06cccb9b616991b55a5b8e97d51103b4bd57
VtrMomesso <vtrmomesso@gmail.com>
2024-03-27 02:57:13 -0300

    Create provinces.py

---
+++ 2week5_functions_list/provinces.py
@@ -0,0 +1,15 @@
+
+
+
+
+def main():
+
+    provinces_list = read_list("provinces.txt")
+
+    print(provinces_list)
+
+    #remove the first item from the list.
+    provinces_list.pop(0)
+
+    provinces_list.pop()
+
