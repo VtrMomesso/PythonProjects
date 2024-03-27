17f2cc389767d52c4a086972287cef16dcf46b7f
VtrMomesso <vtrmomesso@gmail.com>
2024-03-27 03:10:24 -0300

    Update provinces.py

--- 2week5_functions_list/provinces.py
+++ 2week5_functions_list/provinces.py
@@ -42,7 +42,21 @@ def read_list(filename):
     # the lines of text from the text file.
     text_list = []
 
-    
+    # Open the text file for reading and store a reference
+    # to the opened file in a variable named text_file.
+    with open(filename, "rt") as text_file:
+
+        #read the contents of the text file one line at a time.
+        for line in text_file:
+
+            # Remove white space, if there is any,
+            # from the beginning and end of the line.
+            clean_line = line.strip()
+
+            # Append the clean line of text
+            # onto the end of the list.
+            text_list.append(clean_line)
+             
 
 
 if __name__ == "__main__":
