c45ca015979f7029b81eb76e76238b091013c88e
VtrMomesso <vtrmomesso@gmail.com>
2024-03-30 19:27:45 -0300

    Create receipt.py

---
+++ 2week5_functions_list/receipt.py
@@ -0,0 +1,14 @@
+
+
+
+def read_dictionary(filename, key_column_index):
+    """Read the contents of a CSV file into a compound
+    dictionary and return the dictionary.
+
+    Parameters
+        filename: the name of the CSV file to read.
+        key_column_index: the index of the column
+            to use as the keys in the dictionary.
+    Return: a compound dictionary that contains
+        the contents of the CSV file.
+    """
